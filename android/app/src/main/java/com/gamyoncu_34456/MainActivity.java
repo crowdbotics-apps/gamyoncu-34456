21d2b66155b478f9db44db9153b5f84beb7c3791
Crowdbotics <team@crowdbotics.com>
2022-03-27 12:58:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gamyoncu_34456/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gamyoncu_34456;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gamyoncu_34456";
+  }
+}
